6fa8cbbdc5d0ca7cbfdbc013f6fab85f9b61d3ee
Hasan ali <mac@MacBook-Pro-Mac.local>
2026-06-18 13:12:40 +0400

    chore: pydantic structure and docs

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,6 +9,16 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
+
+class Source(BaseModel):
+    ''' Schema for a source used by the agent '''
+    url:str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    ''' Schema for the response from the agent '''
+    answer: str = Field(description="The answer from the agent")
+    sources: List[Source] = Field(default_factory=list, description="The sources used by the agent")
 
 tavily = TavilyClient()
 
@@ -19,19 +32,23 @@ def search(query: str) -> str:
         The search result
     """
     print(f"Searching for {query}")
-    return tavily.search(query)
+    return tavily.search(query, include_domains=["linkedin.com"])
 
 
 llm = ChatOpenAI()
 tools = [search]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages": HumanMessage(content="list the linkedin jobs for ai engineer in uae")})
     print(result)
 
 
 if __name__ == "__main__":
     main()
+
+
+# very important
+# https://docs.langchain.com/oss/python/langchain/structured-output
